e29f1ada990a9de7079919c23a975ac3570fd709
FadiaZitouni <fadda@DESKTOP-6CD2LK3.>
2026-03-07 20:29:21 +0100

    modif gitgnore

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",          // obligatoire pour GitHub Pages
-  basePath:  isProd ? "/fadiazitouni.github.io" : "",
-  assetPrefix: isProd ? "/fadiazitouni.github.io/" : "",
+  basePath:  isProd ? "/out" : "",
+  assetPrefix: isProd ? "/out/" : "",
   images: {
     unoptimized: true,       // obligatoire pour GitHub Pages
   },
