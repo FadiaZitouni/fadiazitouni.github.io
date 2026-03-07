850a34e8eb3f3bac41519abd4c6efe9aebe6cf75
FadiaZitouni <fadda@DESKTOP-6CD2LK3.>
2026-03-07 21:32:08 +0100

    deploy

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",          // obligatoire pour GitHub Pages
-  basePath:  isProd ? "/out" : "/out",
-  assetPrefix: isProd ? "/out/" : "/out/",
+  basePath:  isProd ? "/out" : "",
+  assetPrefix: isProd ? "/out/" : "",
   images: {
     unoptimized: true,       // obligatoire pour GitHub Pages
   },
